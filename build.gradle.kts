6a719a128892295cb24f65b2de4b6cce4cbff59a
Roy Straub <roy.straub@codesquad.nl>
2020-12-01 21:14:31 +0100

    Added initial gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.20"
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+application {
+    mainClass.set("nl.rstraub.kata.kotlin.args.Args")
+}
